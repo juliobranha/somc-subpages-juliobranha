ccd84929fc3b81417d5a4655c45dc19dcbfefbf2
Julio Brana <juliobranha@gmail.com>
2014-03-10 20:39:13 +0000

    Small bug in IE fixed

--- src/somc-subpages-juliobranha/javascript/somc.js
+++ src/somc-subpages-juliobranha/javascript/somc.js
@@ -4,6 +4,7 @@ jQuery(document).ready(function($) {
 
     $( ".pages-tree li .parent" ).click(function(e) {
         e.preventDefault();
+        
         $(this).parent().children('ul').slideToggle( "slow" );
         $(this).toggleClass('collapsed'),
         $(this).toggleClass('expanded');     
@@ -14,16 +15,16 @@ jQuery(document).ready(function($) {
     
     $('.pages-tree li .alpha_order').click(function(e) {
         e.preventDefault();
-        var $sort = this;
+        var $sort = this; 
         var $list = $(this).parent().children("ul");
         var $listLi = $('>li',$list);
         $listLi.sort(function(a, b){
             var keyA = $.trim($(a).text());
             var keyB = $.trim($(b).text());
             if($($sort).hasClass('desc')){
-                return (keyA > keyB) ? 1 : 0;
+                return (keyA > keyB) ? 1 : -1;
             } else {
-                return (keyA < keyB) ? 1 : 0;
+                return (keyA < keyB) ? 1 : -1;
             }
         });
         $.each($listLi, function(index, row){
